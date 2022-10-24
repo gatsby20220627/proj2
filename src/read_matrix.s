70db0430d85789e3908ac8f8f1201c060139ca65
gatsby <gatsby.huang@gmail.com>
2022-10-24 16:52:00 +0800

    Finished Task 7

--- src/read_matrix.s
+++ src/read_matrix.s
@@ -26,17 +26,98 @@
 # ==============================================================================
 read_matrix:
 
-    # Prologue
+    addi sp, sp, -20
+    sw ra, 0(sp)
+    sw s0, 4(sp)        # s0 used for res
+    sw s1, 8(sp)        # s1 used for rows
+    sw s2, 12(sp)       # s2 used for cols
+    sw s3, 16(sp)       # s3 used for fd
 
+    mv s1, a1
+    mv s2, a2
 
+    # open file
+    li a1, 0
+    jal fopen
+    li a1, -1
+    beq a0, a1, error_fopen
+    mv s3, a0
 
+    # read rows
+    mv a1, s1
+    li a2, 4
+    jal fread
+    li a1, 4
+    bne a1, a0, error_fread
 
+    # read cols
+    mv a0, s3
+    mv a1, s2
+    li a2, 4
+    jal fread
+    li a1, 4
+    bne a1, a0, error_fread
 
+    # cal total nums, s1 saved it
+    lw s1, 0(s1)
+    lw s2, 0(s2)
+    mul s1, s1, s2
 
+    # malloc
+    mv a0, s1
+    slli a0, a0, 2
+    jal malloc
+    beqz a0 error_malloc
+    mv s0, a0
 
+    # s2 used as index
+    li s2, 0
+    j loop_end
 
+loop_begin:
+    mv a0, s3
+    slli a1, s2, 2
+    add a1, s0, a1
+    li a2, 4
+    jal fread
+    li a1, 4
+    bne a0, a1, error_fread
+    addi s2, s2, 1
+
+loop_end:
+    blt s2, s1, loop_begin
+
+    # close file
+    mv a0, s3
+    jal fclose
+    li a1, -1
+    beq a0, a1, error_fclose
+
+    # set ret flag
+    mv a0, s0
 
     # Epilogue
-
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    addi sp, sp, 20
 
     jr ra
+
+error_fopen:
+    li a0, 27
+    j exit
+
+error_fread:
+    li a0, 29
+    j exit
+
+error_fclose:
+    li a0, 28
+    j exit
+
+error_malloc:
+    li a0, 26
+    j exit
